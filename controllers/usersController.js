c31295d714293bd7e4a66d65fc4ab1d166ccae2d
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2024-12-30 20:16:14 +0100

    Creació d'un model d'Usuari i d'un controlador per a gestionar el Login i el Signin.

---
+++ controllers/usersController.js
@@ -0,0 +1,83 @@
+const bcrypt = require('bcrypt'); // Per a encriptar contrasenyes.
+const jwt = require('jsonwebtoken'); // Per a generar i verificar JWT (JSON Web Tokens).
+
+const User = require(__dirname + '/../models/User');
+
+// ---------
+//  LOG IN:
+// ---------
+
+exports.loginUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    // Trobar l'usuari a la BD:
+    const user = await User.findOne({username});
+
+    // Comparar contrasenya amb la de la BD:
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(password, user.passwordHash);
+
+    // Si l'usuari no existeix o la contrasenya no és correcta:
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'Invalid username or password'
+        })
+    };
+
+    // Crear el payload del token:
+    const payload = {
+        username: user.username,
+        id: user._id
+    };
+
+    const SECRET = process.env.SECRET;
+    const TOKEN_EXPIRATION = process.env.TOKEN_EXPIRATION;
+
+    // Crear el token:
+    const token = jwt.sign(
+        payload,
+        SECRET,
+        {expiresIn: TOKEN_EXPIRATION}
+    );
+
+    // Retornar el token i les dades de l'usuari:
+    res.json(
+        {
+            username: user.username,
+            userId: user._id,
+            token
+        }
+    );
+}
+
+// ----------
+//  SIGN IN:
+// ----------
+
+exports.registerUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    // Encriptar la contrasenya:
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds); //
+
+    // Crear un nou usuari:
+    const user = new User({
+        username,
+        passwordHash
+    });
+
+    // Guardar-lo a la BD:
+    const savedUser = 
+        await user.save()
+        .catch((error) => {
+            res.status(400).json({
+                error: error.message
+            });
+        }
+    );
+
+    // Retornar el nom de l'usuari creat (sense el "passwordHash"):
+    res.json(savedUser.username);
+}
