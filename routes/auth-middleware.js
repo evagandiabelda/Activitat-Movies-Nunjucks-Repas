52bdd1629264654014bedb9390fac341516a4ef9
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-01-01 20:11:47 +0100

    Correcció per a adaptar els middlewares d'autenticació i que es guarde la info en local.

--- routes/auth-middleware.js
+++ routes/auth-middleware.js
@@ -6,49 +6,40 @@ const verifyToken = (req, res, next) => {
     // Llegir el token del header de la petició:
     const token = req.cookies.access_token;
 
-    // Crear una sessió per defecte amb l'usuari a null:
-    req.session = { user: null};
+    // Si no hi ha token, es crea una sessió per defecte amb l'usuari a null:
+    if (!token) {
+        req.locals.user = null;
+        return next();
+    }
 
     // Validar el token:
     try{
         const verified = jwt.verify(token, process.env.SECRET);
-        req.session.user = verified;
-    }catch(error){
-        return res.render('error', {error});
+        req.locals.user = verified;
+    }catch(err){
+        console.error('Error al verificar el token:', err.message);
+        req.locals.user = null; // Si no hi ha token, no hi ha usuari.
     }
 
     next();
 }
 
+// MIDDLEWARE PER A PASSAR L'USUARI A LES VISTES:
+
+const attachUser = (req, res, next) => {
+    res.locals.user = req.locals.user;
+    next();
+};
+
 // MIDDLEWARE PER COMPROVAR SI L'USUARI ESTÀ LOGUEJAT:
 
 const loggedIn = (req, res, next) => {
-    if (req.session.user){
+    if (req.locals.user){
         next();
     }else{
         res.redirect('/users/login');
     }
 }
-
-// MIDDLEWARE PER PASSAR L'USUARI A LES VISTES:
-
-const attachUser = (req, res, next) => {
-    const token = req.cookies.token;
-
-    if (token) {
-        try {
-            const user = jwt.verify(token, process.env.SECRET);
-            res.locals.user = user; // Fer l'usuari accessible a les vistes.
-        } catch (err) {
-            console.error('Error al verificar el token:', err.message);
-            res.locals.user = null; // Si el token no és vàlid, no hi ha usuari.
-        }
-    } else {
-        res.locals.user = null; // Si no hi ha token, no hi ha usuari.
-    }
-
-    next();
-};
 
 module.exports = {
     verifyToken,
